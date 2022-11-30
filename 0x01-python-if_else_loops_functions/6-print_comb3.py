2f2455cc2aabc3284e497d4617944b13ce003b68
folajo <opaajenifemi@gmail.com>
2022-11-30 23:16:11 +0000

    Python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 
-# Author - Folajo
+for i in range(0, 8):
 
+    for j in range(i + 1, 10):
 
+        print("{}{}, ".format(i, j), end='')
 
-for digit1 in range(0, 10):
-
-    for digit2 in range(digit1 + 1, 10):
-
-        if digit1 == 8 and digit2 == 9:
-
-            print("{}{}".format(digit1, digit2))
-
-        else:
-
-            print("{}{}".format(digit1, digit2), end=", ")
+print("{}".format(89))
